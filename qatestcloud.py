dd942d6bf620139e189f25e25c5f2b9c03cde552
edpyt <edrsa1321s@gmail.com>
2023-06-06 13:08:55 +0500

    Тестовое с тестами в Cloud 1.3

--- qatestcloud.py
+++ qatestcloud.py
@@ -2,16 +2,10 @@ import requests
 
 API_URL = 'https://jsonplaceholder.typicode.com/'
 
-REQUEST_POST_URL = API_URL + 'posts'
+REQUEST_POST_URL = API_URL + 'posts/'
 EXPECTED_COLUMNS = ('userId', 'id', 'title', 'body') 
 EXPECTED_TYPES = (int, int, str, str)
     
-# Проверка столбцов
-def values_in_dict(u_dict):
-    for u_data in EXPECTED_COLUMNS:
-        if u_data not in u_dict:
-            return False
-    return True
 
 def test_get_posts():
     response = requests.get(REQUEST_POST_URL)        
@@ -19,39 +13,56 @@ def test_get_posts():
     assert response.status_code == 200
             
     response_data = response.json()
+    
+    assert len(response_data) == 100  # вместо конкретного числа количество
+                                      # строк в таблице
     # проверка столбцов
-    assert all(values_in_dict(user) for user in response_data)
+    assert all(tuple(user.keys()) == EXPECTED_COLUMNS
+               for user in response_data)
     # проверка данных в столбцах
     assert all(tuple(map(type, user.values())) == EXPECTED_TYPES
                 for user in response_data)
+    
+    
+def test_get_post_from_posts():
+    response = requests.get(REQUEST_POST_URL + '100')
+    response_data =response.json()
+    test_data = {'userId': 10, 'id': 100, 'title': 'at nam consequatur ea labore ea harum',
+                 'body': 'cupiditate quo est a modi nesciunt soluta\nipsa voluptas error itaque dicta in\nautem qui minus magnam et distinctio eum\naccusamus ratione error aut'}
+    
+    assert response.status_code == 200
+    assert response_data == test_data
+    assert tuple(response_data.keys()) == EXPECTED_COLUMNS
+    assert tuple(map(type, response_data.values())) == EXPECTED_TYPES
     
 
 def test_post_posts():
     # Тест правильно введённых данных
     test_data = {'userId': 11, 'id': 101,
-                    'title': 'lorum ipsum',
-                    'body': 'yes, lorum ipsum'}
+                 'title': 'lorum ipsum',
+                 'body': 'yes, lorum ipsum'}
     response = requests.post(REQUEST_POST_URL, test_data)
     
     assert response.status_code == 201
     
     # Тест неправильно введёных данных
     test_data = {'userId': 'ahahhaha', 'id': 'Omeds',
-                    'title': 1,
-                    'body': 5}
-        
+                 'title': 1,
+                 'body': 5}
+    
     response = requests.post(REQUEST_POST_URL, test_data)
     
     response_data = response.json()
     
     assert response.status_code == 201
-    assert isinstance(response_data['body'], str)
-    assert isinstance(response_data['title'], str)
+    assert tuple(response_data.keys()) == EXPECTED_COLUMNS
+    assert tuple(map(type, response_data.values())) == EXPECTED_TYPES
+
 
 def test_delete_from_posts():
-    response = requests.delete(REQUEST_POST_URL)
-
-    response_data = response.json()
+    response = requests.delete(REQUEST_POST_URL + '101')
     
-    assert response.status_code == 404  # 0_0
-    assert len(response_data) == 0        
+    assert response.status_code == 200
+   
+    response_get_data = requests.get(REQUEST_POST_URL).json()
+    assert len(response_get_data) == 100
